21c6b46e4e9322e49dd755132e3f6a1a27a48c27
Courtney Jackson <courtneyjackson@Courtneys-MacBook-Air.local>
2021-09-13 08:20:39 -0700

    test commit

--- client/src/screens/SignUp/SignUp.jsx
+++ client/src/screens/SignUp/SignUp.jsx
@@ -1,6 +1,22 @@
-function SignUp() {
+import "./SignUp.css"
+import { useState } from "react"
+import { useHistory } from "react-router-dom"
+import {signUp} from "../../services/users"
+
+function SignUp(props) {
+
+  const history = useHistory()
+
+  const [form, setForm] = useState({
+    user
+  })
+
+
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1>Hello from Sign up page</h1>
+      </>
+    
   )
 }
 
