1d7e5ec1b4c55522b627dc5c6753875bd1d698f5
jadick <adickja@gmail.com>
2021-09-13 10:21:58 -0400

    testing routes

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,7 @@
+function Footer() {
+  return (
+    <h1>Hello World</h1>
+  )
+}
+
+export default Footer
---
+++ client/src/components/Nav/Nav.jsx
@@ -0,0 +1,7 @@
+function Nav() {
+  return (
+    <h1>Hello World</h1>
+  )
+}
+
+export default Nav
