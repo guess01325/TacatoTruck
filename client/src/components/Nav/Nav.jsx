11b71e3af2faa61e2d5dbae03b35ebeade48887f
jadick <adickja@gmail.com>
2021-09-13 16:49:42 -0400

    footer setup

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -5,8 +5,6 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -87,14 +85,6 @@ const Nav = ({ user }) => {
               {alwaysOptions}
               {user ? authenticatedOptions : unauthenticatedOptions}
             </div>
-            {/* <IconButton
-              edge="start"
-              className={classes.menuButton}
-              color="inherit"
-              aria-label="menu"
-            >
-              <MenuIcon />
-            </IconButton> */}
             <Button>
               <img className={classes.menuButton} src="https://res.cloudinary.com/dy6xpqkkj/image/upload/v1631547003/Tactato%20Truck/clipart775400_vxiz46.png" alt="truck side menu" />
             </Button>
