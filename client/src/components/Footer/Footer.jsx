4171658c07798560b851d0d8572a90a24d85f43e
jadick <adickja@gmail.com>
2021-09-13 11:57:37 -0400

    added initial layout functionality and installed materialUI

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,7 +1,35 @@
+import "./Footer.css";
+
 function Footer() {
   return (
-    <h1>Hello World</h1>
-  )
+    <footer>
+      <a href="https://github.com/Nafff">
+        <img
+          src="https://res.cloudinary.com/dy6xpqkkj/image/upload/v1631547003/Tactato%20Truck/download-1-removebg-preview_1_avlm57.png"
+          alt="github octocat"
+        />
+      </a>
+      <a href="https://github.com/CourtneyAJackson">
+        <img
+          src="https://res.cloudinary.com/dy6xpqkkj/image/upload/v1631547003/Tactato%20Truck/download-1-removebg-preview_1_avlm57.png"
+          alt="github octocat"
+        />
+      </a>
+      <p>Check Meowt</p>
+      <a href="https://github.com/eileen813">
+        <img
+          src="https://res.cloudinary.com/dy6xpqkkj/image/upload/v1631547003/Tactato%20Truck/download-1-removebg-preview_1_avlm57.png"
+          alt="github octocat"
+        />
+      </a>
+      <a href="https://github.com/guess01325">
+        <img
+          src="https://res.cloudinary.com/dy6xpqkkj/image/upload/v1631547003/Tactato%20Truck/download-1-removebg-preview_1_avlm57.png"
+          alt="github octocat"
+        />
+      </a>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,7 +1,35 @@
-function Nav() {
-  return (
-    <h1>Hello World</h1>
-  )
-}
+import './Nav.css'
+import { NavLink } from 'react-router-dom'
 
+const authenticatedOptions = (
+    <>
+        <NavLink className="link" to="/create-taco">Create a' Taco</NavLink>
+        <NavLink className="link" to="/sign-out">Sign Out</NavLink>
+    </>
+)
+const unauthenticatedOptions = (
+    <>
+        <NavLink className="link" to="/sign-up">Sign Up</NavLink>
+        <NavLink className="link" to="/sign-in">Sign In</NavLink>
+    </>
+)
+const alwaysOptions = (
+    <>
+        <NavLink className="link" to="/menu">Menu</NavLink>
+    </>
+)
+const Nav = ({ user }) => {
+        return (
+            <nav>
+                <div className="nav">
+                    <NavLink className="logo" to="/">Tacato Truck</NavLink>
+                    <div className="links">
+                        {user && <div className="link welcome">Welcome, {user.username}</div>}
+                        {alwaysOptions}
+                        {user ? authenticatedOptions : unauthenticatedOptions}
+                    </div>
+                </div>
+            </nav>
+        )
+}
 export default Nav
