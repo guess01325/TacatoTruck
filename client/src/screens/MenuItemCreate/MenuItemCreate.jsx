337dc975db76472bfc88fd18c0c69fc120c6b755
Courtney Jackson <courtneyjackson@Courtneys-MacBook-Air.local>
2021-09-13 13:59:57 -0700

    create taco page

--- client/src/screens/MenuItemCreate/MenuItemCreate.jsx
+++ client/src/screens/MenuItemCreate/MenuItemCreate.jsx
@@ -1,6 +1,82 @@
-function MenuItemCreate() {
+import './MenuItemCreate.css'
+import { useState } from 'react'
+// import { Layout } from '../../components/Layout'
+import { Redirect } from 'react-router-dom'
+import { createMenuItem } from '../../services/menuItems'
+
+
+function MenuItemCreate(props) {
+
+  const [menuItem, setMenuItem] = useState({
+    name: '',
+    imgURL: '',
+    price: '',
+    ingredients: ''
+  })
+
+  const [isCreatedMenuItem, setCreatedMenuItem] = useState(false)
+
+  const handleChange = (event) => {
+    const { name, value } = event.target
+    setMenuItem({
+      ...menuItem,
+      [name]: value
+    })
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    const createdItem = await createMenuItem(menuItem)
+    setCreatedMenuItem({ createdItem })
+  }
+
+  if (isCreatedMenuItem) {
+    return <Redirect to={`/menu`} />
+  }
+
+
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1>Create A Taco</h1>
+      {/* <Layout user={props.user}> */}
+      <form className="createItem-form" onSubmit={handleSubmit}>
+        <input
+          className='create-name'
+          placeholder='Name'
+          required
+          name='name'
+          value={menuItem.name}
+          onChange={handleChange}
+        />
+        <input
+          className="create-price"
+          placeholder='Price'
+          required
+          value={menuItem.price}
+          name='price'
+          onChange={handleChange}
+        />
+        <textarea
+          className='create-ingredients'
+          placeholder='Ingredients'
+          required
+          value={menuItem.ingredients}
+          name='ingredients'
+          onChange={handleChange}
+        />
+        <input
+          className="create-link"
+          placeholder="Link"
+          required
+          value={menuItem.imgURL}
+          name='link'
+          onChange={handleChange}
+        />
+        <button type="submit" className="create-submit-button">Submit</button>
+      </form>
+
+      {/* </Layout> */}
+    </>
   )
 }
 
