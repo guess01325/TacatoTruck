5b1965b42318e40657942da7d51332ebe2195412
Courtney Jackson <courtneyjackson@Courtneys-MacBook-Air.local>
2021-09-13 08:52:04 -0700

    sign up functionality done

--- client/src/screens/SignUp/SignUp.jsx
+++ client/src/screens/SignUp/SignUp.jsx
@@ -8,14 +8,100 @@ function SignUp(props) {
   const history = useHistory()
 
   const [form, setForm] = useState({
-    user
+    username: '',
+    email: '',
+    password: '',
+    passwordConfirmation: '',
+    isError: false,
+    errorMsg: '',
   })
 
+  const handleChange = event => 
+    setForm({
+      ...form,
+      [event.target.name]: event.target.value,
+  })
+  
+  const onSignUp = async (event) => {
+    event.preventDefault()
+    const { setUser } = props
+    try {
+      const user = await signUp(form)
+      setUser(user)
+      history.push('/')
+    } catch (error) {
+      console.log("Error", error)
+      setForm({
+        username: '',
+        email: '',
+        password: '',
+        passwordConfirmation: '',
+        isError: true,
+        errorMsg: 'Invalid Sign Up Information',
+      })
+    }
+
+  }
+
+  const renderError = () => {
+    const toggleForm = form.isError ? 'danger error' : ''
+    if (form.isError) {
+      return (
+        <button type="submit" className={toggleForm}>
+          {form.errorMsg}
+        </button>
+      )
+    } else {
+      return <button type="submit">Sign Up</button>
+    }
+  }
+
+  
+const {username, email, password, passwordConfirmation} = form
 
   return (
-    <>
-      <h1>Hello from Sign up page</h1>
-      </>
+    <div className="signup-form">
+      <h1> Sign Up</h1>
+      <form onSubmit={onSignUp}>
+        <label>Username</label>
+        <input
+          required
+          type="text"
+          name='username'
+          value={username}
+          placeholder='Enter username here'
+          onChange={handleChange}
+        />
+         <label>Email</label>
+        <input
+          required
+          type="text"
+          name='email'
+          value={email}
+          placeholder='Enter email here'
+          onChange={handleChange}
+        />
+         <label>Password</label>
+        <input
+          required
+          type="text"
+          name='password'
+          value={password}
+          placeholder='Enter password here'
+          onChange={handleChange}
+        />
+        <label>Password Confirmation</label>
+        <input
+          required
+          type="text"
+          name='passwordConfirmation'
+          value={passwordConfirmation}
+          placeholder='Confirm password'
+          onChange={handleChange}
+        />
+      {renderError()}
+      </form>
+      </div>
     
   )
 }
