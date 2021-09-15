01532c9d288f3db2ae3e5e4a168c8d2d82dea2d9
jadick <adickja@gmail.com>
2021-09-15 14:22:31 -0400

    styling adjustments for screens and testing new seed

--- client/src/screens/SignUp/SignUp.jsx
+++ client/src/screens/SignUp/SignUp.jsx
@@ -3,6 +3,18 @@ import { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { signUp } from "../../services/users";
 import Layout from "../../components/Layout/Layout";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Paper from "@material-ui/core/Paper";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
 
 function SignUp(props) {
   const history = useHistory();
@@ -48,61 +60,216 @@ function SignUp(props) {
     const toggleForm = form.isError ? "danger" : "";
     if (form.isError) {
       return (
-        <button type="submit" className={toggleForm}>
+        <Button
+          type="submit"
+          className={toggleForm}
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
           {form.errorMsg}
-        </button>
+        </Button>
+      );
+    } else if (password !== passwordConfirmation) {
+      return (
+        <Button
+          type="submit"
+          className={toggleForm}
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          Please use matching passwords
+        </Button>
       );
     } else {
-      return <button type="submit">Sign Up</button>;
+      return (
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          Sign Up
+        </Button>
+      );
     }
   };
+
+  function Copyright(props) {
+    return (
+      <Typography
+        variant="body2"
+        color="text.secondary"
+        align="center"
+        {...props}
+      >
+        {"Copyright © "}
+        <Link color="inherit" href="https://mui.com/">
+          Your Website
+        </Link>{" "}
+        {new Date().getFullYear()}
+        {"."}
+      </Typography>
+    );
+  }
 
   const { username, email, password, passwordConfirmation } = form;
 
   return (
+    // <Layout user={props.user}>
+    //   <div className="signup-form">
+    //     <h1> Sign Up</h1>
+    //     <form onSubmit={onSignUp}>
+    //       <label>Username</label>
+    //       <input
+    //         required
+    //         type="text"
+    //         name="username"
+    //         value={username}
+    //         placeholder="Enter username here"
+    //         onChange={handleChange}
+    //       />
+    //       <label>Email</label>
+    //       <input
+    //         required
+    //         type="text"
+    //         name="email"
+    //         value={email}
+    //         placeholder="Enter email here"
+    //         onChange={handleChange}
+    //       />
+    //       <label>Password</label>
+    //       <input
+    //         required
+    //         type="text"
+    //         name="password"
+    //         value={password}
+    //         placeholder="Enter password here"
+    //         onChange={handleChange}
+    //       />
+    //       <label>Password Confirmation</label>
+    //       <input
+    //         required
+    //         type="text"
+    //         name="passwordConfirmation"
+    //         value={passwordConfirmation}
+    //         placeholder="Confirm password"
+    //         onChange={handleChange}
+    //       />
+    //       {renderError()}
+    //     </form>
+    //   </div>
+    // </Layout>
     <Layout user={props.user}>
-      <div className="signup-form">
-        <h1> Sign Up</h1>
-        <form onSubmit={onSignUp}>
-          <label>Username</label>
-          <input
-            required
-            type="text"
-            name="username"
-            value={username}
-            placeholder="Enter username here"
-            onChange={handleChange}
-          />
-          <label>Email</label>
-          <input
-            required
-            type="text"
-            name="email"
-            value={email}
-            placeholder="Enter email here"
-            onChange={handleChange}
-          />
-          <label>Password</label>
-          <input
-            required
-            type="text"
-            name="password"
-            value={password}
-            placeholder="Enter password here"
-            onChange={handleChange}
-          />
-          <label>Password Confirmation</label>
-          <input
-            required
-            type="text"
-            name="passwordConfirmation"
-            value={passwordConfirmation}
-            placeholder="Confirm password"
-            onChange={handleChange}
-          />
-          {renderError()}
-        </form>
-      </div>
+      <Grid container component="main" sx={{ height: "100vh" }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: "url(https://source.unsplash.com/random)",
+            backgroundRepeat: "no-repeat",
+            backgroundColor: (t) =>
+              t.palette.mode === "light"
+                ? t.palette.grey[50]
+                : t.palette.grey[900],
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              height: "80vh",
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign Up
+            </Typography>
+            <Box component="form" noValidate onSubmit={onSignUp} sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="username"
+                value={username}
+                onChange={handleChange}
+                label="Username"
+                name="username"
+                autoComplete="username"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                value={email}
+                onChange={handleChange}
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                value={password}
+                onChange={handleChange}
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                value={passwordConfirmation}
+                onChange={handleChange}
+                name="passwordConfirmation"
+                label="Confirm Password"
+                type="password"
+                id="passwordConfirmation"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              {renderError()}
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+              <Copyright sx={{ mt: 5 }} />
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
     </Layout>
   );
 }
