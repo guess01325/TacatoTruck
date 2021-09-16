37c9c8b34488239f25d57c35e9dd94eb137b0057
Eileen Olivera <eileenolivera@hotmail.com>
2021-09-16 11:26:53 -0400

    updated styling for App.css & MenuItem.jsx.  Standardized ingredients in seed/data.js

--- client/src/components/MenuItem/MenuItem.jsx
+++ client/src/components/MenuItem/MenuItem.jsx
@@ -1,6 +1,4 @@
 import "./MenuItem.css";
-import { Link as RouterLink } from "react-router-dom";
-import Link from "@material-ui/core/Link";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
@@ -8,6 +6,7 @@ import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
+import { createTheme, ThemeProvider } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
@@ -32,6 +31,13 @@ const useStylesGrid = makeStyles((theme) => ({
   },
 }));
 
+const theme = createTheme({
+  typography: {
+    fontFamily: "Segoe UI",
+    fontSize: 15,
+  },
+});
+
 function MenuItem(props) {
   const classes = useStyles();
   const classesGrid = useStylesGrid();
@@ -55,14 +61,17 @@ function MenuItem(props) {
               component="img"
               alt="item"
               height="140"
+              maxwidth="50"
               image={props.imgURL}
               alt={props.name}
               title={props.name}
             />
             <CardContent className={classes.cardContent}>
-              <Typography gutterBottom variant="h6" component="h2">
-                {props.name}
-              </Typography>
+              <ThemeProvider theme={theme}>
+                <Typography gutterBottom variant="h6" component="h1">
+                  {props.name}
+                </Typography>
+              </ThemeProvider>
             </CardContent>
           </CardActionArea>
           <CardActions>
