a9bca9d3d05db3cf86b2be27bde70c9e342aff00
Courtney Jackson <courtneyjackson@Courtneys-MacBook-Air.local>
2021-09-16 10:58:53 -0700

    styling for sign in and sign up

--- client/src/screens/SignIn/SignIn.jsx
+++ client/src/screens/SignIn/SignIn.jsx
@@ -3,32 +3,9 @@ import { useState } from "react";
 import { signIn } from "../../services/users";
 import { useHistory } from "react-router-dom";
 import Layout from "../../components/Layout/Layout";
-import Avatar from "@material-ui/core/Avatar";
+import {Link} from "react-router-dom"
 import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Link from "@material-ui/core/Link";
-import Paper from "@material-ui/core/Paper";
-import Box from "@material-ui/core/Box";
-import Grid from "@material-ui/core/Grid";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
-
-const useStylesGrid = makeStyles((theme) => ({
-  gridRoot: {
-    flexGrow: 1,
-    margin: 32,
-  },
-  signInCat: {
-    minWidth: "300px",
-
-  }
-
-}));
-
 
 
 function SignIn(props) {
@@ -85,11 +62,13 @@ function SignIn(props) {
       );
     } else {
       return (
+        
         <Button
           type="submit"
           fullWidth
           variant="contained"
           sx={{ mt: 3, mb: 2 }}
+          color="secondary"
         >
           Sign In
         </Button>
@@ -107,7 +86,7 @@ function SignIn(props) {
       >
         {"Copyright © "}
         <Link color="inherit" href="https://mui.com/">
-          Your Website
+          Tacoto Truck
         </Link>{" "}
         {new Date().getFullYear()}
         {"."}
@@ -115,101 +94,46 @@ function SignIn(props) {
     );
   }
 
-  const classesGrid = useStylesGrid();
   return (
     <Layout user={props.user}>
-      <Grid container component="main" sx={{ height: "100vh" }} direction="row">
-        <CssBaseline />
-        <Grid item m className={classesGrid.signInCat}>
-          {/* <Paper elevation={3}> */}
-          <h1> Sign in </h1>
-            <img
-              className="catInHat"
-              src="https://res.cloudinary.com/dy6xpqkkj/image/upload/v1631547003/Tactato%20Truck/istockphoto-833450608-612x612-removebg-preview_ms3oez.png"
-              alt="cat"
-            ></img>
-          {/* </Paper> */}
-        </Grid>
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={7}
-          sx={{
-            backgroundImage: "url(https://source.unsplash.com/random)",
-            backgroundRepeat: "no-repeat",
-            backgroundColor: (t) =>
-              t.palette.mode === "light"
-                ? t.palette.grey[50]
-                : t.palette.grey[900],
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
+      <div className="outterContainer">
+      <div className="formContainer">
+  
+        <h1 className="signInLabel">Sign In</h1>
+        <form onSubmit={onSignIn}>
+          <div className="signInEmail">
+        <label>Email</label>
+        <input
+          required
+          type='text'
+          name='email'
+          value={email}
+          placeholder='Enter Email'
+          onChange={handleChange}
+            />
+          </div>
+          <div className="signInPw"> 
+        <label>Password</label>
+        <input
+          required
+          name='password'
+          value={password}
+          type='password'
+          placeholder='Password'
+          onChange={handleChange}
         />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-          <Box
-            sx={{
-              my: 8,
-              mx: 4,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              height: "80vh",
-            }}
-          >
-            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign in
-            </Typography>
-            <Box component="form" noValidate onSubmit={onSignIn} sx={{ mt: 1 }}>
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                value={email}
-                onChange={handleChange}
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                value={password}
-                onChange={handleChange}
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              />
-              {renderError()}
-              <Grid container>
-                <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Forgot password?
-                  </Link>
-                </Grid>
-                <Grid item>
-                  <Link href="#" variant="body2">
-                    {"Don't have an account? Sign Up"}
-                  </Link>
-                </Grid>
-              </Grid>
-              <Copyright sx={{ mt: 5 }} />
-            </Box>
-          </Box>
-        </Grid>
-      </Grid>
+          </div>
+         
+        {renderError()}
+      </form>
+        </div>
+        <div className="catInHat">
+            <img
+                src="https://res.cloudinary.com/dy6xpqkkj/image/upload/v1631547003/Tactato%20Truck/istockphoto-833450608-612x612-removebg-preview_ms3oez.png"
+                alt="cat"
+            />
+          </div>
+      </div>
     </Layout>
   );
 }
