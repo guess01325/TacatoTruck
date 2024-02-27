c0f81207f67ac6ab43e03771a8d724b4ee535940
guess01325 <guess01325@gmail.com>
2024-02-26 20:35:04 -0500

    fix: refactor hard coded connection string to Heroku config var

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ import express from 'express'
 import cors from 'cors'
 import logger from 'morgan'
 
+const uri = process.env.MONGODB_URI;
+
 const app = express()
 const PORT = process.env.PORT || 3000
 
